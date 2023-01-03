74c3aa9f08c2c0057d4c43aefbe29ca976ad348d
Bogdan <ser.butenko2010@gmail.com>
2023-01-04 00:15:37 +0200

    Work in progress

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\MainPageController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::middleware('guest')->group(function () {
+//    Route::get('register', [RegisterController::class, 'create'])->name('register');
+//    Route::post('register', [RegisterController::class, 'store']);
+//});
+    Route::get('register', [RegisterController::class, 'create'])->name('register');
+    Route::post('register', [RegisterController::class, 'store']);
+
+    Route::get('login', [LoginController::class, 'create'])->name('login');
+    Route::post('login', [LoginController::class, 'store']);
+
+    Route::get('dashboard', [MainPageController::class, 'create']);
+
+
