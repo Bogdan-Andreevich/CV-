7f850ae4e9dda3aabb2e9b709a3b78891ff4712c
Bogdan <ser.butenko2010@gmail.com>
2023-01-05 20:51:03 +0200

    Make validation for name and password, email validation in progress

--- resources/views/authorization/register.blade.php
+++ resources/views/authorization/register.blade.php
@@ -11,25 +11,75 @@
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Create and account
                     </h1>
-                    <form class="space-y-4 md:space-y-6" method="POST" action="{{route('register')}}">
+                    <form class="space-y-4 md:space-y-6" onsubmit ="return validateForm()" method="POST" action="{{route('register')}}">
                         @csrf
                         <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
                             <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required="">
+                            <span id = "blankMsg" style="color:red"> </span>
                         </div>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            <span id = "blankEmail" style="color:red"> </span>
                         </div>
+                            <div>
+                                <script>
+                                    function validateForm() {
 
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                        </div>
-                        <div>
-                            <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                        </div>
+                                        var pw1 = document.getElementById("pswd1").value;
+                                        var pw2 = document.getElementById("pswd2").value;
+                                        var name = document.getElementById("name").value;
+                                        var email = document.getElementById("email").value;
+
+
+
+                                        if(name == "") {
+                                            document.getElementById("blankMsg").innerHTML = "**Fill your name";
+                                            return false;
+                                        }
+
+
+                                        if(name.length < 2) {
+                                            document.getElementById("blankMsg").innerHTML = "**Not enough characters";
+                                            return false;
+                                        }
+
+
+                                        if(!isNaN(name)){
+                                            document.getElementById("blankMsg").innerHTML = "**Only characters are allowed";
+                                            return false;
+                                        }
+
+
+                                        if(pw1 == "") {
+                                            document.getElementById("message1").innerHTML = "**Fill the password please!";
+                                            return false;
+                                        }
+
+                                        if(pw1.length < 8) {
+                                            document.getElementById("message1").innerHTML = "**Password length must be atleast 8 characters";
+                                            return false;
+                                        }
+
+                                        if(pw1 != pw2) {
+                                            document.getElementById("message2").innerHTML = "**Passwords are not same";
+                                            return false;
+                                        }
+                                    }
+                                </script>
+                                <div>
+                                    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="pswd1" id="pswd1" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                                    <span id = "message1" style="color:red"> </span>
+                                </div>
+                                <div>
+                                    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                    <input type="password" name="pswd2" id="pswd2" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                                    <span id = "message2" style="color:red"> </span>
+                                </div>
+
+                            </div>
                         <div class="flex items-start">
                             <div class="flex items-center h-5">
                                 <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <img class="w-100 h-100" style="width: 100%; height: 100%;" src="https://thumbs.dreamstime.com/b/welcome-to-my-shop-words-white-display-soft-pink-pastel-background-192937221.jpg" alt="logo">
+</body>
+</html>
